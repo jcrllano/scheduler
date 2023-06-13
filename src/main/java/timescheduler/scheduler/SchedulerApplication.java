63916e3d044a5d864d8c32d97738b6a9e67c920e
jcrllano <arellanojuan033@gmail.com>
2023-06-13 01:24:12 +0000

    added a comment

--- src/main/java/timescheduler/scheduler/SchedulerApplication.java
+++ src/main/java/timescheduler/scheduler/SchedulerApplication.java
@@ -2,7 +2,7 @@ package timescheduler.scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//main app
 @SpringBootApplication
 public class SchedulerApplication {
 
