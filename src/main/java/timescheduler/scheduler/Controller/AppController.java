efb4d98b3dd9bb50711b3d282c8712bea8c178a5
jcrllano <arellanojuan033@gmail.com>
2023-06-14 03:04:11 +0000

    added content to  app controller

--- src/main/java/timescheduler/scheduler/Controller/AppController.java
+++ src/main/java/timescheduler/scheduler/Controller/AppController.java
@@ -1,8 +1,27 @@
 package timescheduler.scheduler.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import timescheduler.scheduler.Repository.TimeSetRepo;
+import timescheduler.scheduler.Service.UserService;
 
 @Controller
 public class AppController {
-    
+
+    @Autowired 
+    private UserService service;
+
+    @Autowired
+        TimeSetRepo repo;
+      
+    @RequestMapping("/home")
+    public String index(Model model) {
+        var list = service.getAllUserInfo();
+        model.addAttribute("list", list);
+        return "home";
+    }
 }
