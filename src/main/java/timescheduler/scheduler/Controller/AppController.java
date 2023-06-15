6cef8ae6fe7bc9b974d70233d3cd005cb5eb9934
jcrllano <arellanojuan033@gmail.com>
2023-06-15 01:08:26 +0000

    changed the home page name

--- src/main/java/timescheduler/scheduler/Controller/AppController.java
+++ src/main/java/timescheduler/scheduler/Controller/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import timescheduler.scheduler.DTO.UserDto;
 import timescheduler.scheduler.Repository.TimeSetRepo;
 import timescheduler.scheduler.Service.UserService;
 
@@ -17,11 +18,16 @@ public class AppController {
 
     @Autowired
         TimeSetRepo repo;
+    
       
     @RequestMapping("/home")
     public String index(Model model) {
-        var list = service.getAllUserInfo();
-        model.addAttribute("list", list); 
-        return "home";
+        var UserList = service.getAllUserInfo();
+        System.out.println("this is the list from the rep" + UserList);
+        System.out.println();
+        System.out.println();
+        System.out.println("this is the email " + service.get(1).getEmail());
+        model.addAttribute("UserList ", UserList); 
+        return "homeUser";
     }
 }
