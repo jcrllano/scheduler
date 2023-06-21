642040f0f4165899c556d0ec37a71b7b6abc1297
jcrllano <arellanojuan033@gmail.com>
2023-06-21 01:23:21 +0000

    deleted the java classes

--- src/main/java/timescheduler/scheduler/Repository/TimeInventoryRepo.java
+++
@@ -1,9 +0,0 @@
-package timescheduler.scheduler.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import timescheduler.scheduler.DTO.TimeInventory;
-
-public interface TimeInventoryRepo extends JpaRepository<TimeInventory, Long>{
-    
-}
--- src/main/java/timescheduler/scheduler/Repository/UserScheduleTimeRepo.java
+++
@@ -1,9 +0,0 @@
-package timescheduler.scheduler.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import timescheduler.scheduler.DTO.UserSetTime;
-
-public interface UserScheduleTimeRepo extends JpaRepository<UserSetTime, Integer> {
-    
-}
